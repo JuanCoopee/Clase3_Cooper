514f60e3672ea9cb46540f4c0ab7e0f358918a3e
JuanCoopee <160975758+JuanCoopee@users.noreply.github.com>
2024-04-15 17:36:43 -0600

    Add files via upload
    Primer practica del segundo parcial

---
+++ Practica 1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Text = Button1.Text
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Text = Button2.Text
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Text = Button3.Text
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Label1.Text = "Hola clase"
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Label1.Text = "Clase de prgramacion"
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Label1.Text = "Adios"
+    End Sub
+
+End Class
